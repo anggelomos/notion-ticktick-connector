c0db129709529342425b005c0f9d00364e392735
Angelo Mosquera <anggelomos@outlook.com>
2022-07-01 21:39:26 -0500

    Add main function to the lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,30 +1,8 @@
-import logging
-import os
 import json
-from controllers.notion_controller import NotionController
-from controllers.ticktick_controller import TicktickController
-from utilities.task_utilities import TaskUtilities  # OAuth2 Manager
-
+from main import main
 
 def lambda_handler(event, conntext):
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)")
-    notion = NotionController(os.getenv('NT_auth'), notion_version='2021-08-16')
-    ticktick = TicktickController(os.getenv('TT_client_id'),
-                                  os.getenv('TT_client_secret'),
-                                  os.getenv('TT_uri'),
-                                  os.getenv('TT_user'),
-                                  os.getenv('TT_pass'))
-
-    notion.get_active_tasks()
-    ticktick.sync_tasks()
-
-    checked_habits = ticktick.get_checked_habits()
-    notion.check_habits(checked_habits)
-    if not TaskUtilities.are_tasks_synced(notion.active_tasks, ticktick.relevant_tasks):
-        ticktick.complete_tasks(notion)
-        ticktick.delete_tasks(notion)
-        ticktick.update_tasks(notion)
-        ticktick.add_new_tasks(notion)
+    main()
 
     return {
         'statusCode': 200,
--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import logging
 import os
-
 from controllers.notion_controller import NotionController
 from controllers.ticktick_controller import TicktickController
 from utilities.task_utilities import TaskUtilities  # OAuth2 Manager
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)")
-notion = NotionController(os.getenv('NT_auth'), notion_version='2021-08-16')
-ticktick = TicktickController(os.getenv('TT_client_id'),
-                              os.getenv('TT_client_secret'),
-                              os.getenv('TT_uri'),
-                              os.getenv('TT_user'),
-                              os.getenv('TT_pass'))
 
-notion.get_active_tasks()
-ticktick.sync_tasks()
+def main():
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)")
+    notion = NotionController(os.getenv('NT_auth'), notion_version='2021-08-16')
+    ticktick = TicktickController(os.getenv('TT_client_id'),
+                                  os.getenv('TT_client_secret'),
+                                  os.getenv('TT_uri'),
+                                  os.getenv('TT_user'),
+                                  os.getenv('TT_pass'))
 
-checked_habits = ticktick.get_checked_habits()
-notion.check_habits(checked_habits)
-if not TaskUtilities.are_tasks_synced(notion.active_tasks, ticktick.relevant_tasks):
-    ticktick.complete_tasks(notion)
-    ticktick.delete_tasks(notion)
-    ticktick.update_tasks(notion)
-    ticktick.add_new_tasks(notion)
+    notion.get_active_tasks()
+    ticktick.sync_tasks()
+
+    checked_habits = ticktick.get_checked_habits()
+    notion.check_habits(checked_habits)
+    if not TaskUtilities.are_tasks_synced(notion.active_tasks, ticktick.relevant_tasks):
+        ticktick.complete_tasks(notion)
+        ticktick.delete_tasks(notion)
+        ticktick.update_tasks(notion)
+        ticktick.add_new_tasks(notion)
+
+if __name__ == "__main__":
+    main()
