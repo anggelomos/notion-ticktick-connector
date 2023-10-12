9f7cf4c713c7774307a88f9b723b51420e2c180d
Angelo Mosquera <anggelomos@outlook.com>
2023-10-11 19:32:52 -0500

    Fix test env-var names

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,9 @@ from tickthon import TicktickClient
 
 @pytest.fixture(scope="module")
 def notion_client():
-    return NotionClient(os.getenv("NT_auth"))
+    return NotionClient(os.getenv("NT_AUTH"))
 
 
 @pytest.fixture(scope="module")
 def ticktick_client():
-    return TicktickClient(os.getenv("TT_user"), os.getenv("TT_pass"))
+    return TicktickClient(os.getenv("TT_USER"), os.getenv("TT_PASS"))
