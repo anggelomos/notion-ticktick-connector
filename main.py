ca3a22669c33afdb4445ad9e2a28d8769131d1bb
Angelo Mosquera <anggelomos@outlook.com>
2025-07-11 07:17:14 -0500

    Add poetry and refreshed env vars

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import logging
 import os
+from dotenv import load_dotenv
 from src.task_syncer import TaskSyncer
 
 from tickthon import TicktickClient
 from nothion import NotionClient
 
+# Load environment variables from .env file
+load_dotenv()
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)")
 
 
 def main():
-    ticktick = TicktickClient(os.getenv("TT_user"), os.getenv("TT_pass"))
-    notion = NotionClient(os.getenv("NT_auth"))
+    ticktick = TicktickClient(os.getenv("TT_USER"), os.getenv("TT_PASS"))
+    notion = NotionClient(os.getenv("NT_AUTH"), os.getenv("NT_TASKS_DB_ID"))
 
     task_syncer = TaskSyncer(ticktick, notion)
     task_syncer.sync_expenses()
