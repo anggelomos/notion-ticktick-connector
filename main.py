fe816aeb1661bc1c49c504f02bed09dac860ffef
Angelo Mosquera <anggelomos@outlook.com>
2025-07-18 06:55:53 -0500

    Refactor how the tasks are synced

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from src.task_syncer import TaskSyncer
 
-from tickthon import TicktickClient
+from tickthon import TicktickClient, TicktickListIds
 from nothion import NotionClient
 
 # Load environment variables from .env file
@@ -11,16 +11,19 @@ load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)")
 
+notion_tasks_db_id="19541ab983668171baf0d28b0afd4d36"
+ticktick_list_ids = TicktickListIds(INBOX="inbox114478622",
+                                    TODAY_BACKLOG="6616e1af8f08b66b69c7a5c5",
+                                    WEEK_BACKLOG="61c62f198f08c92d0584f678",
+                                    MONTH_BACKLOG="61c634f58f08c92d058540ba",
+                                    WEIGHT_MEASUREMENTS="640c03cd8f08d5a6c4bb32e7")
 
 def main():
-    ticktick = TicktickClient(os.getenv("TT_USER"), os.getenv("TT_PASS"))
-    notion = NotionClient(os.getenv("NT_AUTH"), os.getenv("NT_TASKS_DB_ID"))
+    ticktick = TicktickClient(os.getenv("TT_USER"), os.getenv("TT_PASS"), ticktick_list_ids)
+    notion = NotionClient(os.getenv("NT_AUTH"), notion_tasks_db_id)
 
     task_syncer = TaskSyncer(ticktick, notion)
-    task_syncer.sync_expenses()
-    task_syncer.sync_highlights()
     task_syncer.sync_tasks()
-    task_syncer.add_work_task_tag()
 
 
 if __name__ == "__main__":
--- src/task_syncer.py
+++ src/task_syncer.py
@@ -1,10 +1,8 @@
 import logging
-from typing import Set, Optional, Iterable, List
+from typing import Optional, Iterable
 
 from nothion import NotionClient
-from tickthon import Task, TicktickClient
-from tickthon._config import get_ticktick_ids
-from tickthon.data.ticktick_id_keys import TicktickIdKeys as tik, TicktickFolderKeys as tfK
+from tickthon import Task, TaskType, TicktickClient
 
 
 class TaskSyncer:
@@ -12,90 +10,55 @@ class TaskSyncer:
     def __init__(self, ticktick: TicktickClient, notion: NotionClient):
         self._ticktick = ticktick
         self._notion = notion
-        self._ticktick_tasks: Set[Task] = set(self._ticktick.get_active_tasks())
-        self._notion_tasks: Set[Task] = set(self._notion.get_active_tasks())
-        self.deleted_ticktick_tasks = self._ticktick.get_deleted_tasks()
+        self._ticktick_tasks: list[Task] = self._ticktick.get_active_tasks()
+        self._notion_tasks: list[Task] = self._notion.tasks.get_active_tasks()
+        self._completed_ticktick_tasks = self._ticktick.get_completed_tasks()
 
     def sync_tasks(self):
-        self._get_unsync_tasks()
-        self.sync_ticktick_tasks()
-        self.sync_notion_tasks()
+        ticktick_tasks = self._get_ticktick_tasks()
+        self._sync_ticktick_tasks(ticktick_tasks)
 
-    def sync_expenses(self) -> List[dict]:
-        expenses_synced = []
-        for expense_task, expense_log in self._ticktick.get_expense_logs():
-            expenses_synced.append(self._notion.add_expense_log(expense_log))
-            self._ticktick.complete_task(expense_task)
-        return expenses_synced
+    def _get_ticktick_tasks(self) -> list[Task]:
+        """Get all tasks from ticktick in the active lists"""
+        ticktick_lists = self._ticktick.ticktick_list_ids
+        active_lists = [ticktick_lists.TODAY_BACKLOG, ticktick_lists.WEEK_BACKLOG, ticktick_lists.MONTH_BACKLOG]
+        active_tasks = self._ticktick.get_tasks_by_list(active_lists, TaskType.ALL)
 
-    def sync_highlights(self) -> List[dict]:
-        highlights_synced = []
-        for log in self._ticktick.get_day_logs():
-            highlight_create = None
-            if "highlight" in log.tags:
-                highlight_create = self._notion.add_highlight_log(log)
+        return active_tasks
 
-            if highlight_create:
-                highlights_synced.append(highlight_create)
-        return highlights_synced
+    def _sync_ticktick_tasks(self, ticktick_tasks: list[Task]):
+        """Sync ticktick tasks to notion.
 
-    def _get_unsync_tasks(self):
-        logging.info("Getting unsync tasks.")
-        ticktick_base_unsync_tasks = self._ticktick_tasks - self._notion_tasks
-        notion_base_unsync_tasks = self._notion_tasks - self._ticktick_tasks
-
-        ticktick_etags = {t.ticktick_etag for t in ticktick_base_unsync_tasks}
-        notion_etags_dict = {t.ticktick_etag: t for t in notion_base_unsync_tasks}
-        notion_clean_etags = set(notion_etags_dict.keys()) - ticktick_etags
-
-        ticktick_ids = {t.ticktick_id for t in ticktick_base_unsync_tasks}
-        notion_ids_dict = {t.ticktick_id: t for t in notion_base_unsync_tasks}
-        notion_clean_ids = set(notion_ids_dict.keys()) - ticktick_ids
-
-        self._ticktick_unsync_tasks = ticktick_base_unsync_tasks
-        self._notion_unsync_tasks = set([notion_etags_dict[etag] for etag in notion_clean_etags] +
-                                        [notion_ids_dict[task_id] for task_id in notion_clean_ids])
-
-    def create_notion_tasks(self, task: Task):
-        self._notion.create_task(task)
-        if self.is_task_note(task):
-            self._notion.create_task_note(task)
-
-    def update_notion_tasks(self, task: Task):
-        self._notion.update_task(task)
-        if self.is_task_note(task):
-            if not self._notion.is_task_note_already_created(task):
-                self._notion.create_task_note(task)
+        Args:
+            ticktick_tasks: list[Task] - List of ticktick tasks to sync to notion.
+        """
+        logging.info(f"Syncing ticktick tasks: {ticktick_tasks}")
+        for task in ticktick_tasks:
+            notion_task = self._search_for_task(task, self._notion_tasks)
+            if notion_task and not self._is_task_synced(task, notion_task):
+                self._notion.tasks.update_task(task)
             else:
-                self._notion.update_task_note(task)
+                logging.warning(f"TICKTICK TASK {task.title} WAS NOT SYNCED")
 
-    def delete_notion_tasks(self, task: Task):
-        self._notion.delete_task(task)
-        if self.is_task_note(task):
-            self._notion.delete_task_note(task)
+    def _is_task_synced(self, ticktick_task: Task, notion_task: Task) -> bool:
+        """Check if a task is synced.
 
-    def complete_notion_tasks(self, task: Task):
-        self._notion.complete_task(task)
-        if self.is_task_note(task):
-            self._notion.complete_task_note(task)
+        Args:
+            ticktick_task: Task - Ticktick task to check.
+            notion_task: Task - Notion task to check.
 
-    def sync_ticktick_tasks(self):
-        logging.info(f"Syncing ticktick tasks: {self._ticktick_unsync_tasks}")
-        for task in self._ticktick_unsync_tasks:
-            notion_task = self.search_for_task(task, self._notion_tasks)
-            if notion_task is None:
-                self.create_notion_tasks(task)
-            elif task != notion_task:
-                self.update_notion_tasks(task)
-            else:
-                logging.warning(f"TICKTICK TASK {task.ticktick_id} WITH ETAG {task.ticktick_etag} "
-                                f"WAS NOT SYNCED")
+        Returns:
+            bool: True if all sync fields match between tasks, False otherwise.
+        """
+        return (ticktick_task.project_id == notion_task.project_id and
+                ticktick_task.column_id == notion_task.column_id and
+                ticktick_task.focus_time == notion_task.focus_time)
 
     def sync_notion_tasks(self):
         logging.info("Syncing notion tasks")
         for task in self._notion_unsync_tasks:
-            was_task_deleted = self.search_for_task(task, self.deleted_ticktick_tasks)
-            was_task_updated = self.search_for_task(task, self._ticktick_unsync_tasks)
+            was_task_deleted = self._search_for_task(task, self.deleted_ticktick_tasks)
+            was_task_updated = self._search_for_task(task, self._ticktick_unsync_tasks)
 
             if was_task_deleted:
                 self.delete_notion_tasks(task)
@@ -105,20 +68,17 @@ class TaskSyncer:
                 self.complete_notion_tasks(task)
 
     @staticmethod
-    def is_task_note(tasks: Task) -> bool:
-        return "notes" in tasks.tags
-
-    @staticmethod
-    def search_for_task(search_task: Task, tasks: Iterable[Task]) -> Optional[Task]:
-        logging.info(f"Searching task with id {search_task.ticktick_id} and etag {search_task.ticktick_etag}")
+    def _search_for_task(search_task: Task, tasks: Iterable[Task]) -> Optional[Task]:
+        logging.info(f"Searching task: {search_task.title}")
         for task in tasks:
-            if task.ticktick_etag == search_task.ticktick_etag or task.ticktick_id == search_task.ticktick_id:
+            if task.title == search_task.title:
                 return task
         return None
 
     def add_work_task_tag(self):
-        work_task_list_id = get_ticktick_ids()[tik.LIST_IDS.value][tfK.WORK_TASKS.value]
-        work_reminders_list_id = get_ticktick_ids()[tik.LIST_IDS.value][tfK.WORK_REMINDERS.value]
+        # TODO: Remove this function once the changes are completed.
+        work_task_list_id = self._ticktick.ticktick_list_ids.WORK_TASKS
+        work_reminders_list_id = self._ticktick.ticktick_list_ids.WORK_REMINDERS
         work_tasks = [task for task in self._ticktick_tasks if
                       task.project_id in [work_task_list_id, work_reminders_list_id]]
 
